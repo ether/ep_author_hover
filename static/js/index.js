df0f297b59f3e38ae93d76cd16f5ce44624c7fcd
John McLear <john@mclear.co.uk>
2013-04-07 19:03:22 +0200

    Author Class is always first so we can substr it
    More sanity.

--- static/js/index.js
+++ static/js/index.js
@@ -17,8 +17,9 @@ var showAuthor = {
   },
   authorIdFromClass: function(className){
     if (className.substring(0, 7) == "author-") {
+      className = className.substring(0,49);
       return className.substring(7).replace(/[a-y0-9]+|-|z.+?z/g, function(cc) {
-      if (cc == '-') { return '.'; }
+        if (cc == '-') { return '.'; }
         else if (cc.charAt(0) == 'z') {
           return String.fromCharCode(Number(cc.slice(1, -1)));
         }
