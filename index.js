64ae7551db381001cd2bdd3bcb3d8744a0bd92ce
John McLear <john@mclear.co.uk>
2020-11-22 15:08:59 +0000

    autofixes from Etherpad checkPlugins.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-var settings = require('ep_etherpad-lite/node/utils/Settings');
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
 
 exports.eejsBlock_mySettings = function (hook_name, args, cb) {
   checked_state = 'checked';
-  if (settings.ep_author_hover){
-    if (settings.ep_author_hover.disabledByDefault == true){
+  if (settings.ep_author_hover) {
+    if (settings.ep_author_hover.disabledByDefault == true) {
       checked_state = '';
     }
   }
-  args.content = args.content + eejs.require('ep_author_hover/templates/settings.ejs', {checked : checked_state});
+  args.content += eejs.require('ep_author_hover/templates/settings.ejs', {checked: checked_state});
   return cb();
-}
+};
